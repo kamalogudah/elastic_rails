aede9489a21d7dd6e1a15d5a6f6c498b584d2f71
kamalogudah <mcpaul2058@gmail.com>
2019-02-23 14:48:32 +0300

    Define Index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+  settings do
+    mappings dynamic: false do
+      indexes :author, type: :text
+      indexes :title, type: :text, analyzer: :english
+      indexes :body, type: :text, analyzer: :english
+      indexes :tags, type: :text, analyzer: :english
+      indexes :published, type: :boolean
+    end
+  end
 end
